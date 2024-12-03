ac28797d8f015f6d714946bb4220818ca46a1818
BichraiX <amine.chraibi@polytechnique.edu>
2024-12-03 19:02:40 +0100

    finetuned model for barbell detection further

--- train_barbell_detector.py
+++ train_barbell_detector.py
@@ -4,19 +4,19 @@ import numpy as np
 import os
 
 # Initialize YOLO model
-model = YOLO('best.pt')
+model = YOLO('models/best_barbell_detector.pt')
 
 # Fine-tune YOLO model
 model.train(
     data="/users/eleves-a/2022/amine.chraibi/AI-Deadlift-Analysis/dataset/data.yaml",  
-    epochs=1000,           
-    batch=64,             
+    epochs=250,           
+    batch=128,             
     imgsz=640            
 )
 
 metrics = model.val(data="/users/eleves-a/2022/amine.chraibi/AI-Deadlift-Analysis/dataset/data.yaml") 
-print(metrics) 
 
+model.export(format="onnx")
 
 
 
